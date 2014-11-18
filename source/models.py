2b8bff74b900b7b2406e2146804437558bc6f21f
Rishabh Bhargava <rishabh.bhargava93@gmail.com>
2014-11-18 01:12:43 +0000

    Small changes to make SGD and GBM work

--- source/models.py
+++ source/models.py
@@ -6,8 +6,7 @@ James Robert Lloyd 2013
 
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.linear_model import SGDClassifier
+from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 
@@ -56,7 +55,7 @@ class GBM_c():
         self.n_estimators = n_estimators
         self.learn_rate = learn_rate
         self.max_depth = max_depth
-        self._model = GradientBoostingClassifier(loss='deviance', n_estimators=n_estimators, learn_rate=learn_rate, max_depth=max_depth)
+        self._model = GradientBoostingClassifier(loss='deviance', n_estimators=n_estimators, learning_rate=learn_rate, max_depth=max_depth)
 
     def description(self):
         return 'GBM %s %s %s' % (self.n_estimators, self.learn_rate, self.max_depth)
@@ -88,7 +87,7 @@ class SGD_c():
         return 'SGD %s %s' % (self.penalty, self.loss)
 
     def predict_p(self, X_train, y_train, X_test): 
-        return self._model.fit(X_train, y_train).predict_proba(X_test)
+        return self._model.fit(X_train, y_train).predict_proba(X_test)[:,-1]
 
 class KNN_c():
 
@@ -109,7 +108,6 @@ list_of_classifiers = [GaussianNaiveBayes_c()] + \
                       [LogisticRegression_c(C=C, penalty=penalty) for C in [0.001, 0.01, 0.1, 1, 10, 100] for penalty in ['l1', 'l2']] + \
                       [SGD_c(loss=loss, penalty=penalty) for loss in ['log'] for penalty in ['l1', 'l2', 'elasticnet']] + \
                       [RandomForest_c(n_tree, criterion) for n_tree in [100,200,300,400,500] for criterion in ['gini', 'entropy']] + \
+                      [Tree_c(min_samples_leaf, criterion) for min_samples_leaf in [1,5,10,20,50] for criterion in ['gini', 'entropy']] + \
                       [GBM_c(n_estimators, learn_rate, max_depth) for n_estimators in [100,300,500] for learn_rate in [0.0001,0.001,0.01,0.1,1] for max_depth in [1,3,5]] + \
-                      [GBM_c(n_estimators, learn_rate, max_depth) for n_estimators in [10,25,50] for learn_rate in [0.0001,0.01,1] for max_depth in [1,2]] + \
-                      [Tree_c(min_samples_leaf, criterion) for min_samples_leaf in [1,5,10,20,50] for criterion in ['gini', 'entropy']]
-    
+                      [GBM_c(n_estimators, learn_rate, max_depth) for n_estimators in [10,25,50] for learn_rate in [0.0001,0.01,1] for max_depth in [1,2]] 
