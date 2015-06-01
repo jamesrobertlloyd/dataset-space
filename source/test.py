d195fd8748ba8def627ae2e727395aee608952ec
Rishabh Bhargava <rishabh.bhargava93@gmail.com>
2015-06-01 17:51:38 +0100

    Beating Trees + Rf with Logitic Regression

--- source/test.py
+++ source/test.py
@@ -3,6 +3,7 @@ from pylab import *
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn. tree import DecisionTreeClassifier
 from utils.data import split_into_folds
 from utils.pyroc import AUC
 from models import *
@@ -26,20 +27,26 @@ def create_plane(n, k):
 This method creates a k dimensional object in an n dimensional space and returns p points around it with correct labels
 '''
 def yield_points(n,k,p):
-	a = np.random.rand(n)
+	a = np.ones(n)
 	#print a
 	order = np.random.permutation(n)[:k]
 	X = []
 	y = []
 	A = np.random.rand(n,k)
+	#A = np.ones((n,k))
 	for i in xrange(p):
-		x = (2*np.random.rand(n) - 1)[order]
+		# x = [round()(2*np.random.rand(n) - 1)[order]]
+		x = [1 if np.random.rand() > 0.5 else -1 for _ in range(n)]
 		temp = np.asarray([np.dot(A[j,:], x) for j in range(n)])
 		if len(X) == 0:
 			X = temp
 		else:
 			X = np.vstack((X, temp))
 		#print X
+		#temp = x
+		#type(temp)
+		if np.random.rand() < 0.1:
+			temp = -temp
 		if np.dot(temp,a) > 0:
 			y = np.append(y, 1)
 		else:
@@ -72,6 +79,7 @@ def get_the_plots(tups):
 	ratios = []
 	rf_scores = []
 	lr_scores = []
+	tree_scores = []
 	for i in xrange(len(tups)):
 		n,k,p = tups[i]
 		X,y = yield_points(n,k,p)
@@ -88,13 +96,25 @@ def get_the_plots(tups):
 		score = np.mean([AUC(logregmodel.predict_p(fold['X_train'], fold['y_train'], fold['X_test']), fold['y_test']) for fold in data['folds']])
 		lr_scores.append(score)
 		print score
-		ratios.append(n*1.0/k)
-	plot(ratios,rf_scores,'r')
-	plot(ratios, lr_scores, 'g')
+
+		dtmodel = Tree_c(10, 'gini')
+		score = np.mean([AUC(dtmodel.predict_p(fold['X_train'], fold['y_train'], fold['X_test']), fold['y_test']) for fold in data['folds']])
+		tree_scores.append(score)
+		print score
+
+		ratios.append(p)
+	line1, = plot(ratios,rf_scores,'r', label='RF')
+	line1, = plot(ratios, lr_scores, 'g', label='LR')
+	line1, = plot(ratios, tree_scores, 'k', label='Tree')
+	legend()
+	xlabel('Number of points in the dataset')
+	ylabel('Performance score')
 	show()
 
 
-get_the_plots([(1000,x+1,100) for x in range(1000)])
+get_the_plots([(1000, 1000, 10),(1000, 1000, 20)] + [(1000,1000,21+2*x) for x in range(4)] + [(1000,1000,20+10*(x+1)) for x in range(8)])
+
+#get_the_plots([(100,100,5+ 2*(x+1)) for x in range(30)])
 
 '''d = {}
 d['X'] = X
